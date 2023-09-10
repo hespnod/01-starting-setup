052d1bfde075031660783a68b23305175369655f
rishabh <hespnod2348@gmai.com>
2023-09-10 16:43:09 +0530

    added remove functionality using DOM

--- src/components/Expenses/ExpenseDetails.jsx
+++ src/components/Expenses/ExpenseDetails.jsx
@@ -2,6 +2,8 @@ import './ExpenseItem.css'
 import Card from '../UI/Card'
 
 const ExpenseDeatils = (props)=>{
+
+
     return(
         <Card className="expense-item">
             <div className="expense-item__location">{props.location}</div>
@@ -9,6 +11,9 @@ const ExpenseDeatils = (props)=>{
                 <h2>{props.title}</h2>
                 <div className='expense-item__price'>{props.amount} Rs</div>
             </div> 
+            <button onClick={(ele)=>{const rem = ele.target.parentElement;
+                rem.remove();
+            }} className='delete'>Delete</button>
         </Card>
     );
 }
