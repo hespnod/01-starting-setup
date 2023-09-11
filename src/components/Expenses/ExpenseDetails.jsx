d77343a9a69db3fedb36d25fd60a7cb2b7d0521d
rishabh <hespnod2348@gmai.com>
2023-09-12 00:09:06 +0530

    added a button which makes amount to 100Rs

--- src/components/Expenses/ExpenseDetails.jsx
+++ src/components/Expenses/ExpenseDetails.jsx
@@ -1,19 +1,22 @@
 import './ExpenseItem.css'
 import Card from '../UI/Card'
+import { useState } from 'react';
 
 const ExpenseDeatils = (props)=>{
 
+    const[amount,setAmount] = useState(props.amount);
+    const handleClick = ()=>{
+        setAmount(100);
+    }
 
     return(
         <Card className="expense-item">
             <div className="expense-item__location">{props.location}</div>
             <div className='expense-item__description'>
                 <h2>{props.title}</h2>
-                <div className='expense-item__price'>{props.amount} Rs</div>
+                <div className='expense-item__price'>{amount} Rs</div>
             </div> 
-            <button onClick={(ele)=>{const rem = ele.target.parentElement;
-                rem.remove();
-            }} className='delete'>Delete</button>
+            <button onClick={handleClick}>Make 100</button>
         </Card>
     );
 }
